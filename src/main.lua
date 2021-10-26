32755879c65ec511426ce7cf3b72055a903d3bc2
RigidStudios <34319439+RigidStudios@users.noreply.github.com>
2021-10-26 14:01:49 +0400

    fix stuff and better print

--- src/main.lua
+++ src/main.lua
@@ -2,12 +2,12 @@ day = 0
 infected = 1
 notInfected = 9
 
-while infected < 10 do
-  math.randomseed(os.time() - 220 / 35 * 1000 / 37)
+while infected < 10 and infected > 0 and notInfected > 0 do
+  math.randomseed(math.floor(os.time() - 220 / 35 * 1000 / 37 * 100000))
   
   chanceOfInfection = math.random(1, 100) -- 75%
   chanceOfDeath = math.random(1, 100) -- 55%
-  chanceOfNaturalDeath = math.random(1, 100) -- 35%
+  chanceOfNaturalDeath = math.random(1, 100) -- 20%
 
   if chanceOfInfection <= 75 then
     infected = infected + 1
@@ -18,11 +18,12 @@ while infected < 10 do
     infected = infected - 1
   end
   
-  if chanceOfNaturalDeath <= 35 then
+  if chanceOfNaturalDeath <= 20 then
     notInfected = notInfected - 1
   end
   
-  print("Day: " .. day .. " Infected: " .. infected .. " Not Infected: " .. notInfected)
+  print(string.format("--- Day %s ---\nInfected: %s\nNot Infected: %s\n", day, infected, notInfected))
+
   
   day = day + 1
 end
